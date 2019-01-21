f344f18c941b7516f15a1ff6aa567ef36d7d6cc1
Jesus rodriguez <jdrgpersonal@gmail.com>
2019-01-21 12:41:39 -0500

    --

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-badge.Badge",
+    "file": "plugins/cordova-plugin-badge/www/badge.js",
+    "pluginId": "cordova-plugin-badge",
+    "clobbers": [
+      "cordova.plugins.notification.badge"
+    ]
+  },
+  {
     "id": "cordova-plugin-browsertab.BrowserTab",
     "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
     "pluginId": "cordova-plugin-browsertab",
@@ -57,6 +65,14 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
     "id": "cordova-plugin-geolocation.Coordinates",
     "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
     "pluginId": "cordova-plugin-geolocation",
@@ -98,11 +114,29 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-universal-links-plugin.universalLinks",
-    "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
-    "pluginId": "cordova-universal-links-plugin",
+    "id": "cordova-plugin-local-notification.LocalNotification",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+    "pluginId": "cordova-plugin-local-notification",
     "clobbers": [
-      "universalLinks"
+      "cordova.plugins.notification.local"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Core",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Util",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "merges": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
     ]
   },
   {
@@ -131,65 +165,38 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
+    "id": "cordova-universal-links-plugin.universalLinks",
+    "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
+    "pluginId": "cordova-universal-links-plugin",
     "clobbers": [
-      "device"
+      "universalLinks"
     ]
   },
   {
-    "id": "cordova-plugin-badge.Badge",
-    "file": "plugins/cordova-plugin-badge/www/badge.js",
-    "pluginId": "cordova-plugin-badge",
-    "clobbers": [
-      "cordova.plugins.notification.badge"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Core",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Util",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-badge": "0.8.8",
   "cordova-plugin-compat": "1.2.0",
   "cordova-plugin-browsertab": "0.2.0",
   "cordova-plugin-buildinfo": "2.0.2",
   "cordova-plugin-camera": "4.0.3",
   "cordova-plugin-customurlscheme": "4.3.0",
+  "cordova-plugin-device": "2.0.2",
   "cordova-plugin-geolocation": "4.0.1",
   "cordova-plugin-inappbrowser": "3.0.0",
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-universal-links-plugin": "1.2.1",
+  "cordova-plugin-local-notification": "0.9.0-beta.2",
   "cordova-plugin-network-information": "2.0.1",
   "cordova-plugin-screen-orientation": "3.0.1",
-  "cordova-plugin-device": "2.0.2",
-  "cordova-plugin-badge": "0.8.8",
-  "cordova-plugin-local-notification": "0.9.0-beta.2"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-universal-links-plugin": "1.2.1",
+  "es6-promise-plugin": "4.2.2"
 };
 // BOTTOM OF METADATA
 });
